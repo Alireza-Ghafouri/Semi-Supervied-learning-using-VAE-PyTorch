85341637ae86fbca208b1e05729cc7e11b7d235f
Alireza Ghafouri <alirezaghafouri98@gmail.com>
2024-05-20 00:04:33 +0330

    Update config & main

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from dataloader.utils import split_data
 from torch.utils.data import DataLoader
 
 
-config = OmegaConf.load("config\config.yaml")
+config = OmegaConf.load(os.path.join('config' , 'config.yaml'))
 
 os.makedirs(config.paths.weights_root, exist_ok = True)
 os.makedirs(config.paths.report_root, exist_ok = True)
@@ -56,68 +56,69 @@ vae = DCVAE(image_channels= config.data.in_channels,
 
 vae.to(device)
 
-# vae_optimizer = Adam(vae.parameters(),
-#                      lr=config.learning.learning_rate)
+vae_optimizer = Adam(vae.parameters(),
+                     lr=config.learning.vae.learning_rate)
 
-# vae_scheduler = ExponentialLR(optimizer = vae_optimizer,
-#                               gamma = config.learning.schd_gamma)
+vae_scheduler = ExponentialLR(optimizer = vae_optimizer,
+                              gamma = config.learning.vae.schd_gamma)
 
-# vae_trainer = Trainer(net= vae, 
-#                       train_dataloader= full_trainloader, 
-#                       test_dataloader= testloader,
-#                       optimizer= vae_optimizer, 
-#                       scheduler= vae_scheduler, 
-#                       device= device
-#                     )
+vae_trainer = Trainer(net= vae, 
+                      train_dataloader= full_trainloader, 
+                      test_dataloader= testloader,
+                      optimizer= vae_optimizer, 
+                      scheduler= vae_scheduler, 
+                      device= device
+                    )
 
-# vae_trainer.train(num_epochs= config.learning.vae_num_epochs,
-#                   vae_weight= 1, 
-#                   cls_weight= 0, 
-#                   cnt_weight= 0,
-#                   save_rec_path= config.paths.rec_results,
-#                   )
+vae_trainer.train(num_epochs= config.learning.vae.num_epochs,
+                  vae_weight= 1, 
+                  cls_weight= 0, 
+                  cnt_weight= 0,
+                  save_rec_path= config.paths.rec_results,
+                  )
 
-# vae_trainer.save_weights(path= os.path.join( config.paths.weights_root,'vae_svhn.pth' ) )
+vae_trainer.save_weights(path= os.path.join( config.paths.weights_root,'vae_svhn.pth' ) )
+vae_trainer.save_loss_plot(path = os.path.join( config.paths.report_root,'vae_svhn.png' ) )
 
 # vae.load_state_dict(torch.load(os.path.join( config.paths.weights_root,'vae_svhn.pth' )))
 # print("vae weights loaded...\n")
 
 
-latent_mapper = LatentMapper(y_dim = config.model.vae_latent_dim,
-                             z_dim = config.model.cls_latent_dim,
-                             hidden_dims = config.model.lm_hidden_dims,
-                             contrastive_margin = config.loss.contrastive_margin, 
-                             contrastive_similarity = config.loss.contrastive_similarity
-                             )
-latent_mapper.to(device)
+# latent_mapper = LatentMapper(y_dim = config.model.vae_latent_dim,
+#                              z_dim = config.model.cls_latent_dim,
+#                              hidden_dims = config.model.lm_hidden_dims,
+#                              contrastive_margin = config.loss.contrastive_margin, 
+#                              contrastive_similarity = config.loss.contrastive_similarity
+#                              )
+# latent_mapper.to(device)
 
 
-net = NET(vae, latent_mapper, num_classes=config.data.num_classes)
-net.to(device)
+# net = NET(vae, latent_mapper, num_classes=config.data.num_classes)
+# net.to(device)
 
-net_optimizer = Adam(net.parameters(),
-                    lr=config.learning.learning_rate
-                 )
+# net_optimizer = Adam(net.parameters(),
+#                     lr=config.learning.net.learning_rate
+#                  )
 
-net_scheduler = ExponentialLR(optimizer = net_optimizer,
-                              gamma = config.learning.schd_gamma)
+# net_scheduler = ExponentialLR(optimizer = net_optimizer,
+#                               gamma = config.learning.net.schd_gamma)
 
-net_trainer = Trainer(net= net, 
-                      train_dataloader= full_trainloader, 
-                      test_dataloader= testloader,
-                      optimizer= net_optimizer, 
-                      scheduler= net_scheduler, 
-                      device= device
-                    )
+# net_trainer = Trainer(net= net, 
+#                       train_dataloader= full_trainloader, 
+#                       test_dataloader= testloader,
+#                       optimizer= net_optimizer, 
+#                       scheduler= net_scheduler, 
+#                       device= device
+#                     )
 
-net_trainer.train(num_epochs= config.learning.net_num_epochs,
-                  vae_weight= config.loss.vae_term_weight, 
-                  cls_weight= config.loss.classification_term_weight, 
-                  cnt_weight= config.loss.contrastive_term_weight,
-                  )
+# net_trainer.train(num_epochs= config.learning.net.num_epochs,
+#                   vae_weight= config.loss.vae_term_weight, 
+#                   cls_weight= config.loss.classification_term_weight, 
+#                   cnt_weight= config.loss.contrastive_term_weight,
+#                   )
 
-net_trainer.get_accuracy()
+# net_trainer.get_accuracy()
 
-net_trainer.save_loss_plot(path = os.path.join( config.paths.report_root,'step2_try1.png' ) )
+# net_trainer.save_loss_plot(path = os.path.join( config.paths.report_root,'step2_try1.png' ) )
 
 # vae_trainer.save_rec_images(path= config.paths.rec_results, filename= 0, mode='test')
