afc77951d815a870e2ea0be0aa01d8e050765add
Alireza Ghafouri <alirezaghafouri98@gmail.com>
2024-06-04 02:25:15 +0330

    rename dataset.py to datasets.py + update getitem methods

--- dataloader/datasets.py
+++ dataloader/datasets.py
@@ -1,43 +1,36 @@
-import torch
-import torchvision
-import torchvision.transforms as transforms
 from torchvision.datasets import SVHN, CIFAR10
 from torch.utils.data import Dataset
 
 class SVHNDataset(Dataset):
-    def __init__(self, mode):
+    def __init__(self, mode, transform= None):
         super(SVHNDataset, self).__init__()
         self.data = SVHN(root='./data', split=mode, download=True)
-        self.transform = transforms.Compose([
-            transforms.Resize(64),  # Resize images to 64x64
-            transforms.ToTensor(),
-        ])
+        self.transform = transform
 
     def __getitem__(self, index):
 
         image, label = self.data[index]
-        # Applying transforms on image
-        image = self.transform(image)
-        return image, label
+        if self.transform is not None:
+            image = self.transform(image)
+
+        return image, label, index
 
     def __len__(self):
         return len(self.data)
 
 class CIFAR10Dataset(Dataset):
-    def __init__(self, is_train):
+    def __init__(self, is_train, transform= None):
         super(CIFAR10Dataset, self).__init__()
         self.data = trainset = CIFAR10(root='./data', train= is_train, download=True)
-        self.transform = transforms.Compose([
-            transforms.Resize(64),  # Resize images to 64x64
-            transforms.ToTensor(),
-        ])
+        self.transform = transform
 
     def __getitem__(self, index):
 
         image, label = self.data[index]
-        # Applying transforms on image
-        image = self.transform(image)
-        return image, label
+        if self.transform is not None:
+            image = self.transform(image)
+
+        return image, label, index
 
     def __len__(self):
         return len(self.data)
@@ -51,4 +44,4 @@ class MyDataset(Dataset):
         return len(self.samples)
 
     def __getitem__(self, idx):
-        return self.samples[idx], self.labels[idx]
+        return self.samples[idx], self.labels[idx], idx
