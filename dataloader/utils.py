8d8390232cc92734431276a1e9ed132bcbd9548c
Alireza Ghafouri <alirezaghafouri98@gmail.com>
2024-06-06 13:36:30 +0330

    add confidence treshold for pseudo labelling

--- dataloader/utils.py
+++ dataloader/utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 from torch.utils.data import Subset
 import torch
+import torch.nn.functional as F
 from tqdm.auto import tqdm
 from training.utils import apply_transformations
 
@@ -22,7 +23,7 @@ def split_data(full_trainset, labeled_ratio):
 
     return labeled_trainset, unlabeled_trainset
 
-def create_pseudo_labeled_dataset(net, unlabeled_trainloader, mannual_dataset, device, transforms):
+def create_pseudo_labeled_dataset(net, unlabeled_trainloader, mannual_dataset, device, transforms, confidence_threshold):
     pseudo_labeled_images = []
     pseudo_labels = []
 
@@ -36,17 +37,25 @@ def create_pseudo_labeled_dataset(net, unlabeled_trainloader, mannual_dataset, d
             images= images.to(device)
             
             logits = net(images)[-1]
-            predicted_labels = torch.argmax(logits, 1)
+            # predicted_labels = torch.argmax(logits, 1)
             
-            pseudo_labeled_images.extend(copy_images)
-            pseudo_labels.extend(predicted_labels.tolist())
+            # pseudo_labeled_images.extend(copy_images)
+            # pseudo_labels.extend(predicted_labels.tolist())
+
+            probabilities = F.softmax(logits, dim=1)
+            max_probs, predicted_labels = torch.max(probabilities, dim=1)
+            
+            for img, label, prob in zip(copy_images, predicted_labels, max_probs):
+                if prob >= confidence_threshold:
+                    pseudo_labeled_images.append(img)
+                    pseudo_labels.append(int(label))
 
     # Convert pseudo-labels to int
-    pseudo_labels = [int(label) for label in pseudo_labels]
+    # pseudo_labels = [int(label) for label in pseudo_labels]
 
     # Create pseudo-labeled dataset
     pseudo_labeled_trainset = mannual_dataset(pseudo_labeled_images, pseudo_labels)
-
+    print(f'\n %{round( 100 * len(pseudo_labeled_trainset) / len(unlabeled_trainloader.dataset) , 1)} of unlabeled samples pseudo labeled!\n')
     return pseudo_labeled_trainset
 
 def selective_collate(batch):
